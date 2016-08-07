623ab08decac56ed0e1550ffd38c31e2ee724eaa
Kenny <sangemi@daum.net>
2016-08-07 18:56:24 +0900

    3.1042
    wave.bonzero.com 카톡링크 추가

--- resources/views/boon/site/wave6.blade.php
+++ resources/views/boon/site/wave6.blade.php
@@ -367,14 +367,14 @@
 				// // 카카오톡 링크 버튼을 생성합니다. 처음 한번만 호출하면 됩니다.
 				Kakao.Link.createTalkLinkButton({
 					container: '#kakao-link-btn',
-					label: '카카오링크 샘플에 오신 것을 환영합니다.',
+					label: '인터파크 정보유출',
 					image: {
-						src: 'http://dn.api1.kage.kakao.co.kr/14/dn/btqaWmFftyx/tBbQPH764Maw2R6IBhXd6K/o.jpg',
+						src: '/site/wave/images/kakao_send_file.png',
 						width: '200',
 						height: '150'
 					},
 					webButton: {
-						text: '카카오 디벨로퍼스',
+						text: '소비자운동 참여',
 						url: 'http://wave.boonzero.com/wave/6' // 앱 설정의 웹 플랫폼에 등록한 도메인의 URL이어야 합니다.
 					}
 
