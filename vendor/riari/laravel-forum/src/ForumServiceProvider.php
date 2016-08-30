efd6ce2e4c594929036827c57aa21335390952c0
Kenny <sangemi@daum.net>
2016-08-30 16:20:42 +0900

    3.5.2
    게시판에 Role 설정

--- vendor/riari/laravel-forum/src/ForumServiceProvider.php
+++ vendor/riari/laravel-forum/src/ForumServiceProvider.php
@@ -10,7 +10,6 @@ use Riari\Forum\Models\Post;
 use Riari\Forum\Models\Thread;
 use Riari\Forum\Models\Observers\PostObserver;
 use Riari\Forum\Models\Observers\ThreadObserver;
-use App\Policies\Forum;
 
 class ForumServiceProvider extends ServiceProvider
 {
